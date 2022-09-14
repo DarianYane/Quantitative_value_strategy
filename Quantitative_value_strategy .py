b7403437fd943d45b88dcb425fb44909abfe1368
Darian Yane <darianyane@yahoo.com.ar>
2022-09-14 20:59:12 -0300

    Update Quantitative_value_strategy .py

--- Quantitative_value_strategy .py
+++ Quantitative_value_strategy .py
@@ -9,7 +9,7 @@ From there, we will calculate recommended trades for an equal-weight portfolio o
 import numpy as np #The Numpy numerical computing library
 import pandas as pd #The Pandas data science library
 import requests #The requests library for HTTP requests in Python
-import xlsxwriter #The XlsxWriter libarary for 
+import xlsxwriter #The XlsxWriter library 
 import math #The Python math module
 from scipy import stats #The SciPy stats module
 
